79cc3b00965649b200dce2fee965498756434d17
chanpreet <chanpreet1999@gmail.com>
2019-11-10 04:27:30 +0530

    added some more scripts

---
+++ HelloClient.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.IO;
+using UnityEngine;
+
+public class HelloClient : MonoBehaviour
+{
+    public HelloRequester _helloRequester;
+    WebCamTexture webCamTexture;
+    public static byte[] photoBytes;
+
+    private void Start()
+    {
+        _helloRequester = new HelloRequester();
+
+        webCamTexture = new WebCamTexture();
+        GetComponent<Renderer>().material.mainTexture = webCamTexture; //Add Mesh Renderer to the GameObject to which this script is attached to
+        webCamTexture.Play();
+    }
+
+    public void sendReq()
+    {
+        _helloRequester.Start();
+    }
+
+    private void OnDestroy()
+    {
+        _helloRequester.Stop();
+    }
+
+    public void takePhoto()
+    {
+        StartCoroutine(TakePhoto());
+    }
+
+    IEnumerator TakePhoto()  // Start this Coroutine on some button click
+    {
+
+        // NOTE - you almost certainly have to do this here:
+
+        yield return new WaitForEndOfFrame();
+
+
+        Texture2D photo = new Texture2D(webCamTexture.width, webCamTexture.height);
+        photo.SetPixels(webCamTexture.GetPixels());
+        photo.Apply();
+
+        //Encode to a PNG
+        photoBytes = photo.EncodeToPNG();
+        Debug.Log(photoBytes);
+        //Write out the PNG. Of course you have to substitute your_path for something sensible
+        //File.WriteAllBytes("photo.png", photoBytes);
+        Debug.Log("Photu taken");
+
+        sendReq();
+
+        //yield return new WaitForSeconds(4);
+
+        //OnDestroy();
+        _helloRequester = new HelloRequester();
+    }
+}
---
+++ HelloRequester.cs
@@ -0,0 +1,86 @@
+﻿using AsyncIO;
+using NetMQ;
+using NetMQ.Sockets;
+using System.Runtime.InteropServices;
+using UnityEngine;
+using static Classification;
+
+/// <summary>
+///     Example of requester who only sends Hello. Very nice guy.
+///     You can copy this class and modify Run() to suits your needs.
+///     To use this class, you just instantiate, call Start() when you want to start and Stop() when you want to stop.
+/// </summary>
+public class HelloRequester : RunAbleThread
+{
+    /// <summary>
+    ///     Request Hello message to server and receive message back. Do it 10 times.
+    ///     Stop requesting when Running=false.
+    /// </summary>
+    //Color32 img;
+    public string message = "";
+    public static string msg = "";
+
+    protected override void Run()
+    {
+        ForceDotNet.Force(); // this line is needed to prevent unity freeze after one use, not sure why yet
+        using (RequestSocket client = new RequestSocket())
+        {
+            client.Connect("tcp://192.168.43.129:5555");
+
+            //for (int i = 0; i < 10 && Running; i++)
+            //{
+                Debug.Log("Sending Hello");
+                //Debug.Log(img32);
+                //client.SendFrame(Color32ArrayToByteArray(img32));
+                //client.SendFrame("frick");
+                client.SendFrame(HelloClient.photoBytes);
+                
+                // ReceiveFrameString() blocks the thread until you receive the string, but TryReceiveFrameString()
+                // do not block the thread, you can try commenting one and see what the other does, try to reason why
+                // unity freezes when you use ReceiveFrameString() and play and stop the scene without running the server
+//                string message = client.ReceiveFrameString();
+//                Debug.Log("Received: " + message);
+                message = "";
+                bool gotMessage = false;
+                while (Running)
+                {
+                    gotMessage = client.TryReceiveFrameString(out message); // this returns true if it's successful
+                    if (gotMessage) break;
+                }
+
+            if (gotMessage)
+            {
+                Debug.Log("Received " + message);
+                msg = message;
+            }
+            //}
+        }
+
+        NetMQConfig.Cleanup(); // this line is needed to prevent unity freeze after one use, not sure why yet
+    }
+
+    private static byte[] Color32ArrayToByteArray(Color32[] colors)
+    {
+        if (colors == null || colors.Length == 0)
+            return null;
+
+        int lengthOfColor32 = Marshal.SizeOf(typeof(Color32));
+        int length = lengthOfColor32 * colors.Length;
+        byte[] bytes = new byte[length];
+
+        GCHandle handle = default(GCHandle);
+        try
+        {
+            handle = GCHandle.Alloc(colors, GCHandleType.Pinned);
+            System.IntPtr ptr = handle.AddrOfPinnedObject();
+            Marshal.Copy(ptr, bytes, 0, length);
+        }
+        finally
+        {
+            if (handle != default(GCHandle))
+                handle.Free();
+        }
+
+        return bytes;
+    }
+}
